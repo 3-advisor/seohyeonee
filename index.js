8413c637e41f91b5b253a8cfc05f2142d21aed09
Heejung Park <cho2hol@gmail.com>
2019-07-02 11:41:56 +0900

    개발 환경 구축 #7 - 테스트 페이지 생성

--- index.js
+++ index.js
@@ -5,21 +5,28 @@ const mongoose = require('mongoose');
 const LineBot = require('./src/line/LineBot');
 const Restaurant = require('./src/model/Restaurant');
 
-const LINE_API_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
+const LINE_API_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || 'DUMMY_LINE_CHANNEL_ACCESS_TOKEN';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI);
+if (process.env.NODE_ENV == 'production') {
+    mongoose.connect(process.env.MONGODB_URI);
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-    Restaurant.find(function (err, list) {
-        if (err) return res.status(500).send({
-            error: 'database failure'
+    if (process.env.NODE_ENV == 'production') {
+        // TODO: 정식 웹페이지로 변경 필요.
+        Restaurant.find(function (err, list) { // 데이터 임시 출력
+            if (err) return res.status(500).send({
+                error: 'database failure'
+            });
+            res.json(list);
         });
-        res.json(list);
-    });
+    } else if (process.env.NODE_ENV == 'development') {
+        res.sendFile(path.join(__dirname, '/src/views/test.html'));
+    }
 });
 
 app.post('/line-webhook', express.json(), function (req, res) {
