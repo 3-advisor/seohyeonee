7cfd0f1b3a9ad41d2656e106c0a1a865d5189051
mgcation <mgcation@naver.com>
2019-07-30 01:50:53 +0900

    개발 환경 구축 : 로컬 mongo db 사용
    local mongoDB 에서 테스트 할 수 있도록 환경을 구축하였습니다.
    
    (추후 DB의 개발서버를 구축하면 사용되지 않을 듯함)

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const LINE_API_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI);
+
+if (process.env.DB_ENV == 'local') {
+    const args = process.argv.slice(2);
+    if (args.length >= 2) {
+        process.env.MONGODB_URI = `mongodb://localhost:${args[0]}/${args[1]}`
+    }
+}
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    const ERROR_MSG_DB_NOT_CONNECTED = `DB not connected. It may not work. (process.env.MONGODB_RUI = ${process.env.MONGODB_URI})`;
+    const RED_MSG_START = `\x1b[41m`;
+    const RED_MSG_END = `\x1b[0m`;
+    console.error(`${RED_MSG_START}${ERROR_MSG_DB_NOT_CONNECTED}${RED_MSG_END}`);
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
