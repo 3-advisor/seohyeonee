9a80b50c95fd8ea2aeceb107e20c0fb932d2dd51
mgcation <mgcation@naver.com>
2019-08-02 01:06:11 +0900

    로컬 디비 실행 방법 변경
    # before
    npm script 에서 포트, db이름 입력
    
    # after
    local.env 파일에서 LOCAL_URL 설정

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const mongoose = require('mongoose');
 const LineBot = require('./src/line/LineBot');
 const Restaurant = require('./src/model/Restaurant');
 
-const LINE_API_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
+dotenv.config({ path: './local.env' });
+
+const LINE_API_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || 'DUMMY_LINE_CHANNEL_ACCESS_TOKEN';
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = (process.env.DB_ENV === 'local') ? process.env.MONGODB_LOCAL_URI : process.env.MONGODB_URI;
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI);
+
+if (MONGODB_URI) {
+    mongoose.connect(MONGODB_URI);
+} else {
+    const ERROR_MSG_DB_NOT_CONNECTED = `DB not connected. It may not work. (process.env.MONGODB_RUI = ${process.env.MONGODB_URI})`;
+    const RED_MSG_START = `\x1b[41m`;
+    const RED_MSG_END = `\x1b[0m`;
+    console.error(`${RED_MSG_START}${ERROR_MSG_DB_NOT_CONNECTED}${RED_MSG_END}`);
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
