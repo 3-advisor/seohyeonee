0ac0de8d65237b8ca83048f47afe7a41102c3ef0
mgcation <mgcation@naver.com>
2019-09-18 02:34:36 +0900

    방어코드 주석 추가

--- src/line/__test__/LineBot.test.js
+++ src/line/__test__/LineBot.test.js
@@ -1,7 +1,10 @@
 const LineBot = require('../LineBot');
 const lineBot = new LineBot();
 
-
+/*
+ * lint: no-undef 규칙을 피하기 위해 정의되지 않은 메서드를 빈 메서드로 정의합니다.
+ * jest가 실행 될 때는 해당 메서드가 정의된 채로 실행됩니다.
+ */
 if (!global.it) {
   global.it = () => {
     // empty
