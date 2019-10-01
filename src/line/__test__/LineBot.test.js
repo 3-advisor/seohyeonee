2e80c1ae82f735f23567b85dec048fb8a99e0775
mgcation <mgcation@naver.com>
2019-10-02 00:29:47 +0900

    lint: no-undef 관련 방어코드 제거

--- src/line/__test__/LineBot.test.js
+++ src/line/__test__/LineBot.test.js
@@ -1,22 +1,6 @@
 const LineBot = require('../LineBot');
 const lineBot = new LineBot();
 
-/*
- * lint: no-undef 규칙을 피하기 위해 정의되지 않은 메서드를 빈 메서드로 정의합니다.
- * jest가 실행 될 때는 해당 메서드가 정의된 채로 실행됩니다.
- */
-if (!global.it) {
-  global.it = () => {
-    // empty
-  };
-  global.describe = () => {
-    // empty
-  };
-  global.expect = () => {
-    // empty
-  };
-}
-
 
 class TestUtil {
   static runSimpleTarget(targetObj) {
@@ -24,8 +8,8 @@ class TestUtil {
       if (Object.prototype.hasOwnProperty.call(targetObj, key)) {
         const expectTarget = key;
         const toBeTarget = targetObj[key];
-        global.it(`expect[${expectTarget}] => toBe[${toBeTarget}]`, () => {
-          global.expect(lineBot.extractCommand(expectTarget)).toBe(toBeTarget);
+        it(`expect[${expectTarget}] => toBe[${toBeTarget}]`, () => {
+          expect(lineBot.extractCommand(expectTarget)).toBe(toBeTarget);
         });
       }
     }
@@ -33,8 +17,8 @@ class TestUtil {
 }
 
 
-global.describe('Check Utility', () => {
-  global.describe('Command 추출하기', () => {
+describe('Check Utility', () => {
+  describe('Command 추출하기', () => {
     const testTarget = {};
     testTarget['/뭐먹지 /뭔가 / 먹 / 어야 /함'] = '뭐먹지';
     testTarget['뭐먹지 /뭔가 / 먹 / 어야 /함'] = null;
